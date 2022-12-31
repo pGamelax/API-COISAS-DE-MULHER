cda50bdf67b90fff8cf875118d3091e495f022ad
Pedro Gamela <pedrogamela2003@gmail.com>
2022-12-31 07:42:44 -0300

    test

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -5,6 +5,7 @@ const login = async (req, res) => {
     try {
         const { email, password } = req.body
 
+        console.log(email, password)
         const employee = await authService.loginService(email);
 
         if (!employee) {
